bcce73201f1391578c706dcaadc3fffb431e98af
Bert Temme <btemme@hotmail.com>
2023-03-07 11:18:28 +0100

    normalize normal vector

--- src/triangulator/PolygonExtensions.cs
+++ src/triangulator/PolygonExtensions.cs
@@ -10,7 +10,7 @@ namespace Triangulate
             var vect1 = polygon.ExteriorRing.Points[1].ToVector3() - polygon.ExteriorRing.Points[0].ToVector3();
             var vect2 = polygon.ExteriorRing.Points[2].ToVector3() - polygon.ExteriorRing.Points[0].ToVector3();
             var vectProd = Vector3.Cross(vect1, vect2);
-            return vectProd;
+            return vectProd / vectProd.Length();
         }
     }
 }
