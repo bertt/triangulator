4b7ac0fedbc63bdd4e4daa7fc3ca64af53280212
Sergey Kemaikin <ksi@cgis.io>
2024-01-29 14:41:50 +0200

    improve tests

--- src/triangulator.tests/NormalTests.cs
+++ src/triangulator.tests/NormalTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System.Numerics;
+using System.Text;
 using Wkx;
 
 namespace Triangulate.Tests;
@@ -9,7 +11,7 @@ public class NormalTests
     {
         // arrange
         var wkt = "POLYGON Z ((-75.51889015799998 39.15624977500005 17.71,-75.51880883299998 39.15605855000007 17.71,-75.51882902999995 39.156053351000025 17.71,-75.51874978099994 39.155858297000066 17.71,-75.51882395499996 39.155839280000066 17.71,-75.51889671399994 39.15601041700006 17.71,-75.51894566199996 39.15599799200004 17.71,-75.51903664399998 39.15621200700008 17.71,-75.51889015799998 39.15624977500005 17.71))";
-        var polygon = (Polygon)Geometry.Deserialize<WktSerializer>(wkt);
+        var polygon = (Polygon)Geometry.Deserialize<WktSerializer>(Encoding.UTF8.GetBytes(wkt));
 
         // act
         var normal = polygon.GetNormal();
@@ -23,7 +25,7 @@ public class NormalTests
     {
         // arrange
         var wkt = "POLYGON Z ((0 0 0, 0 1 0, 0 2 0, 0 3 0, 1 3 0, 2 3 0, 3 3 0, 3 2 0, 3 1 0, 3 0 0, 2 0 0, 1 0 0, 0 0 0))";
-        var polygon = (Polygon)Geometry.Deserialize<WktSerializer>(wkt);
+        var polygon = (Polygon)Geometry.Deserialize<WktSerializer>(Encoding.UTF8.GetBytes(wkt));
 
         // act
         var normal = polygon.GetNormal();
