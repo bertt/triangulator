65e6ef1738203eb4d8c62e7d8808e97c7552ab29
Sergey Kemaikin <ksi@cgis.io>
2024-02-08 15:37:34 +0200

    normal calculation via Newell's method

--- src/triangulator/PolygonExtensions.cs
+++ src/triangulator/PolygonExtensions.cs
@@ -8,64 +8,43 @@ namespace Triangulate
     {
         public static Vector3 GetNormal(this Polygon polygon)
         {
-            // calculating normal vector whith skipping the consecutive duplicate vertices
-            double epsilon = 1e-8;
+            // normal calculation via Newell's method
+            double sumX = 0, sumY = 0, sumZ = 0;
+            double 
+                x1 = (double)polygon.ExteriorRing.Points[0].X,
+                y1 = (double)polygon.ExteriorRing.Points[0].Y,
+                z1 = (double)polygon.ExteriorRing.Points[0].Z,
+                x2, y2, z2;
             
-            double startX = (double)polygon.ExteriorRing.Points[0].X;
-            double startY = (double)polygon.ExteriorRing.Points[0].Y;
-            double startZ = (double)polygon.ExteriorRing.Points[0].Z;
-           
-            for (int i = 1; i < polygon.ExteriorRing.Points.Count-1; i++)
+            for (int i = 0; i < polygon.ExteriorRing.Points.Count-2; i++)
             {
-                // find the first vector for cross product
-                // skip point if it duplicates ptStart
-                double v1X = (double)polygon.ExteriorRing.Points[i].X - startX;
-                double v1Y = (double)polygon.ExteriorRing.Points[i].Y - startY;
-                double v1Z = (double)polygon.ExteriorRing.Points[i].Z - startZ;
-                
-                double v1Length = Math.Sqrt(v1X*v1X + v1Y*v1Y + v1Z*v1Z);
-                if (v1Length < epsilon)
-                    continue;
+                x2 = (double)polygon.ExteriorRing.Points[i+1].X;
+                y2 = (double)polygon.ExteriorRing.Points[i+1].Y;
+                z2 = (double)polygon.ExteriorRing.Points[i+1].Z;
 
-                // the first vector found
-                // get the second vector and calculate cross product
-                // select the best cross product: max cross product
-                double maxProdLen = 0.0;
-                double maxCrossProdX = 0.0;
-                double maxCrossProdY = 0.0;
-                double maxCrossProdZ = 0.0;
+                sumX += (y1 - y2) * (z1 + z2);
+                sumY += (z1 - z2) * (x1 + x2);
+                sumZ += (x1 - x2) * (y1 + y2);
 
-                for (int j = i+1; j < polygon.ExteriorRing.Points.Count-1; j++)
-                {
-                    // skip point if it duplicates ptStart
-                    double v2X = (double)polygon.ExteriorRing.Points[j].X - startX;
-                    double v2Y = (double)polygon.ExteriorRing.Points[j].Y - startY;
-                    double v2Z = (double)polygon.ExteriorRing.Points[j].Z - startZ;
-
-                    double v2Length = Math.Sqrt(v2X*v2X + v2Y*v2Y + v2Z*v2Z);
-                    if (v2Length < epsilon)
-                        continue;
-
-                    // the second vector found
-                    // calculate and get the best cross product
-                    double crossProdX = v1Y*v2Z - v1Z*v2Y;
-                    double crossProdY = v1Z*v2X - v1X*v2Z;
-                    double crossProdZ = v1X*v2Y - v1Y*v2X;
-
-                    double prodLen = Math.Sqrt(crossProdX*crossProdX + crossProdY*crossProdY + crossProdZ*crossProdZ);
-                    if (prodLen > maxProdLen) {
-                        maxProdLen = prodLen;
-                        maxCrossProdX = crossProdX;
-                        maxCrossProdY = crossProdY;
-                        maxCrossProdZ = crossProdZ;
-                    }
-                }
-
-                var maxProd = new Vector3((float)maxCrossProdX, (float)maxCrossProdY, (float)maxCrossProdZ);
-                return Vector3.Normalize(maxProd);
+                x1 = x2;
+                y1 = y2;
+                z1 = z2;
             }
+            
+            x2 = (double)polygon.ExteriorRing.Points[0].X;
+            y2 = (double)polygon.ExteriorRing.Points[0].Y;
+            z2 = (double)polygon.ExteriorRing.Points[0].Z;
 
-            return new Vector3(0, 0, 0);
+            sumX += (y1 - y2) * (z1 + z2);
+            sumY += (z1 - z2) * (x1 + x2);
+            sumZ += (x1 - x2) * (y1 + y2);
+
+            sumX /= (polygon.ExteriorRing.Points.Count-1);
+            sumY /= (polygon.ExteriorRing.Points.Count-1);
+            sumZ /= (polygon.ExteriorRing.Points.Count-1);
+            
+            var normal = new Vector3((float)sumX, (float)sumY, (float)sumZ);
+            return Vector3.Normalize(normal);
         }
     }
 }
